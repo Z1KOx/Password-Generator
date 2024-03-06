f4f8a356632e673cbc53ea7406a6a3eae2d5eef8
Z1KOx <119358594+Z1KOx@users.noreply.github.com>
2024-03-06 23:40:54 +0100

    slightly improved performance

--- src/main.cpp
+++ src/main.cpp
@@ -17,14 +17,14 @@ void Generator::getNum()
     std::mt19937_64 gen{ seed };
     std::uniform_int_distribution<int> range{ 0, 91 };
 
-    m_num = range(gen);
+    m_num = std::move(range(gen));
 }
 
 int main()
 {
     SetConsoleOutputCP(CP_UTF8);
 
-    const std::vector<std::string> letters{ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+    const std::vector<std::string_view> letters{ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
                                              "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5",
                                              "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
                                              "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "\"",
