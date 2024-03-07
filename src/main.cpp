009b1e5bfae09262c7abed0e2ff449d116227b7c
Z1KOx <119358594+Z1KOx@users.noreply.github.com>
2024-03-07 13:23:25 +0100

    Added user input + customizable user input length

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <random>
 #include <vector>
 #include <windows.h>
+#include <string>
 
 class Generator
 {
@@ -20,23 +21,48 @@ void Generator::getNum()
     m_num = std::move(range(gen));
 }
 
+unsigned int getLength()
+{
+    unsigned int lengthInput{};
+    std::cout << "How much you want to enter: ";
+    std::cin >> lengthInput;
+
+    return lengthInput;
+}
+
 int main()
 {
     SetConsoleOutputCP(CP_UTF8);
 
-    const std::vector<std::string_view> letters{ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+    std::vector<std::string> passwordChain;
+    const std::vector<std::string> letters{ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
                                              "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5",
                                              "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
                                              "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "\"",
                                              "§", "$", "%", "&", "/", "(", ")", "=", "?", "`", "²", "³", "{", "[", "]", "}",
                                              "+", "*", "-", "_", "@", "<", ">", "|", "°", "^", ";", ",", "." };
 
+    unsigned int length{ getLength() };
+    std::string contentInput{};
     Generator gen;
-    for (unsigned int i{ 0 }; i < letters.size(); ++i)
+
+    for (unsigned int i{ 0 }; i < length; ++i)
     {
-        gen.getNum();
-            
-        std::cout << letters[gen.m_num];
+        std::cout << "Enter name: ";
+        std::getline(std::cin >> std::ws, contentInput);
+
+        passwordChain.clear();
+
+        for (const auto& letter : letters)
+        {
+            gen.getNum();
+            passwordChain.push_back(letter + letters[gen.m_num]);
+        }
+
+        std::cout << contentInput << ": ";
+        for (const auto& password : passwordChain)
+            std::cout << password;
+        std::cout << '\n';
     }
 
     std::cin.get();
