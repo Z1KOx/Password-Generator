f33b52a0ab838e262219f7d75bfdd683f2177358
Z1KOx <119358594+Z1KOx@users.noreply.github.com>
2024-03-12 20:33:51 +0100

    Colored tooltips

--- src/gui.cpp
+++ src/gui.cpp
@@ -338,6 +338,7 @@ void gui::Render() noexcept
 <<<<<<< HEAD
 			ImGui::Text("0.0.1.0");
 =======
+			ImGui::Text("0.0.0.9");
 >>>>>>> 1919212e118a63ec2d6a132d0488897cb50bc349
 
 			ImGui::PopStyleColor(1);
